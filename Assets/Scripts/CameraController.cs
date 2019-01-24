a605475a9705973e65aca5fbb839326ab6a5e5ab
Raunak Kondiboyina <kondaraunak@gmail.com>
2019-01-25 00:00:23 +0530

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	public GameObject cube;
+	
+	private Vector3 offset;
+	private Vector3 offsetcube;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		offsetcube = transform.position - cube.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		//transform.position = player.transform.position + offset;
+		transform.position = cube.transform.position + offsetcube;
+	}
+}
